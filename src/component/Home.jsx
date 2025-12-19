1ad12b036de91fde9f2d3b589f6d24d56f02b766
abhisheknavath3-sketch <abhisheknavath3@gamil.com>
2025-12-19 16:56:51 +0530

    latest

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -219,7 +219,7 @@ function Home() {
                   </div>
                 </div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 py-10 bg-gray-800 ">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 py-10 bg-gray-800 ">
                   {dishes.map((item, index) => (
                     <div
                       key={index}
