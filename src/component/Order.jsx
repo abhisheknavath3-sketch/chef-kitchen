31a996b2e73b62ac03ba3eb07ba607792a1a6755
abhisheknavath3-sketch <abhisheknavath3@gamil.com>
2026-01-08 12:45:46 +0530

    latest

--- src/component/Order.jsx
+++ src/component/Order.jsx
@@ -31,10 +31,10 @@ function Order() {
 
   return (
     <div
-      className="
+      className="absolute
     bg-gray-900 text-white flex flex-col
      top-0 right-0 z-50
-    w-full h-full
+    w-90 h-full
     sm:h-screen
     p-4 sm:p-6
   "
